6e91c8d9c73d47364bfeb81b23318a7b271ee3f2
interactive-sequence-2417 <jiyonj@hotmail.com>
2020-03-20 04:19:02 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -72,6 +72,6 @@ class Owner
   end
 
   def list_pets
-    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+    "I have #{self.dogs.count} dogs, and #{self.cats.count} cats."
   end
 end
